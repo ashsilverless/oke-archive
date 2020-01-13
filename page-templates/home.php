5ded19923e69cbd4f8cf0d581598e15d3cc62b84
Ash - SIlverless <ash@silverless.co.uk>
2020-01-13 16:14:09 +0000

    FIX - Fullwidth Panel Test

--- page-templates/home.php
+++ page-templates/home.php
@@ -165,12 +165,12 @@ $fullwidthImage = get_sub_field('background_image');?>
     <div class="col">
         <div class="container boxed-content last cols-16-8">
             <div class="col info-panel">
-                <?php if( have_rows('info_panel') ):
-                while( have_rows('info_panel') ): the_row(); ?>
+                <!--<?php if( have_rows('info_panel') ):
+                while( have_rows('info_panel') ): the_row(); ?>-->
                     <div class="heading-wrapper">
                         <?php get_template_part('template-parts/panel-icon');?>
                         <h3 class="heading heading__md">
-                            <?php the_sub_field('title');?>
+                            <?php the_sub_field('title');?>test
                         </h3>
                     </div>
                     <div class="content">
@@ -179,7 +179,7 @@ $fullwidthImage = get_sub_field('background_image');?>
                            <?php the_sub_field('button_text');?>
                         </a>
                     </div>
-                <?php endwhile; endif;?>
+                <!--<?php endwhile; endif;?>-->
             </div>
 
         </div>
