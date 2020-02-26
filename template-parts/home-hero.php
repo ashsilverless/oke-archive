5065bae8ee30e46d6d86e1baadc3faeb5532cb49
Ash - SIlverless <ash@silverless.co.uk>
2020-02-26 13:48:07 +0000

    DEV - home hero video

--- template-parts/home-hero.php
+++ template-parts/home-hero.php
@@ -12,27 +12,50 @@ if( get_field('hero_type') !== 'slider'):
 
 <div class="hero home <?php the_field('hero_height');?>" style="background-image: url(<?php echo $heroImage['url']; ?>); background-color: <?php echo $heroColor; ?>;">
 
-    <div class="container cols-3-12-6-3">
-        <div class="col"></div>
+	<?php
+
+	$video = get_field("hero_video");
+
+	if($video):
+		$videoImage = get_field('video_background_image');
+		?>
+
+		<div class="wrapper-video" style="background-image: url(<?php echo $videoImage['url']; ?>);">
+			<!--<video width="auto" height="auto" autoplay src="<?php the_field('hero_video'); ?>"></video>-->
+
+			<video id="heromovie" preload autoplay loop>
+			 <source src="<?php the_field('hero_video'); ?>" />
+			</video>
+
+
+
+
+		</div>
+
+	<?php endif; ?>
+
+
+
+    <div class="container cols-offset3-18">
 		<div class="col">
-		    <div class="hero__content">
-                <div class="inner-section">
+
+                <!--<div class="inner-section">
                     <h1 class="heading heading__md heading__light mt0 mb0"><em><?php the_field('hero_copy');?></em></h1>
-                </div>
+                </div>-->
                 <div class="inner-section">
-                    <?php get_template_part('inc/img/ode', 'logo-top');?>
+                    <!--<?php get_template_part('inc/img/ode', 'logo-top');?>-->
                     <?php get_template_part('inc/img/ode', 'logo-bottom');?>
                 </div>
                 <div class="inner-section">
-                    <h2 class="heading heading__lg heading__light mt0 mb0 slow-fade"><em>
+                    <h1 class="heading heading__xl heading__light mt0 mb0 slow-fade"><em>
                         <?php the_field('hero_heading');?></em>
-                    </h2>
+                    </h1>
                 </div>
                <div class="buttons">
                    <a href="#explore"  class="explore">Explore Now</a>
                    <a href="/enquire">Enquire</a>
                </div>
-		    </div>
+
 		</div>
 	</div>
 
