908ee65a0dbae829fafd36ab0cac814b711bb5ea
rc-mei <45292307+rc-mei@users.noreply.github.com>
2020-03-26 16:53:31 +0000

    About Page Update

--- page-templates/flex-blog-sidebar.php
+++ page-templates/flex-blog-sidebar.php
@@ -11,7 +11,7 @@ $term = get_queried_object();?>
 <?php get_template_part("template-parts/left-hero"); ?>
 
 <div class="outer-wrapper mt3">
-    <div class="container cols-15-9 border-split">
+    <div class="container cols-offset-3-12-6 border-split">
         <div class="col">
             <?php
             if( have_rows('flexible_layout') ):
