d73ca8b763cf4170198bdfad41c3068d0c79d36a
rc-mei <45292307+rc-mei@users.noreply.github.com>
2020-03-30 15:33:01 +0100

    About Page Update

--- page-templates/flex-blog-sidebar.php
+++ page-templates/flex-blog-sidebar.php
@@ -12,7 +12,7 @@ $term = get_queried_object();?>
 
 <div class="outer-wrapper mt3">
     <div class="container cols-offset-3-12-6 border-split">
-        <div class="col">
+        <div class="col pl3 pr3">
             <?php
             if( have_rows('flexible_layout') ):
             while ( have_rows('flexible_layout') ) : the_row();
