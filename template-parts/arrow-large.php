b21cef8aaedf8264b5ed199d451b49a358960ce7
rc-mei <45292307+rc-mei@users.noreply.github.com>
2020-04-06 14:04:17 +0100

    Arrow update

--- template-parts/arrow-large.php
+++ template-parts/arrow-large.php
@@ -1,4 +1,3 @@
-<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-viewBox="0 0 19.5 40" style="enable-background:new 0 0 19.5 40;" xml:space="preserve">
-<path class="st0" d="M19.5,20L0,0c0,0,6,11,6,20S0,40,0,40L19.5,20z"/>
+<svg xmlns="http://www.w3.org/2000/svg" width="15.557" height="15.558" viewBox="0 0 15.557 15.558">
+  <path id="Union_1" data-name="Union 1" d="M-1289,17291v-10h-10v-1h11v11Z" transform="translate(13143.113 -11300.273) rotate(45)" fill="#fff"/>
 </svg>
