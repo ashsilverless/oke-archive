10759bf53867b89533fbdc76c51206348a386534
rc-mei <45292307+rc-mei@users.noreply.github.com>
2020-04-06 11:51:43 +0100

    Panel Icon Update

--- template-parts/panel-icon.php
+++ template-parts/panel-icon.php
@@ -1,5 +1,7 @@
+<?php $thisicon = get_sub_field( 'icon')["url"];?>
+<?php if($thisicon): ?>
 <div class="panel-icon">
-    <?php $thisicon = get_sub_field( 'icon')["url"];
-    $thisicon   = explode("/wp-content/", $thisicon)[1];?>
+    <?php $thisicon   = explode("/wp-content/", $thisicon)[1];?>
     <?php echo file_get_contents("./wp-content/" . $thisicon, FILE_USE_INCLUDE_PATH); ?>    
 </div>
+<?php endif;?>
